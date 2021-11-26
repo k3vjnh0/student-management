59a3a885f1475eee4ef44388e23109aacfc3362b
k3vjnh0 <duynguyen272@gmail.com>
2021-11-27 06:33:59 +0700

    add student code

--- main.py
+++ main.py
@@ -2,12 +2,8 @@ import os
 import csv
 
 
-def clear():
-    os.system("clear")
-
-
 def menu_display():
-    clear()
+    os.system("clear")
     print("Actions list:")
     print("1 - Print")
     print("2 - Add")
@@ -21,16 +17,69 @@ def menu_display():
 
 
 def student_display(students):
-    print("\t+--------+--------------------+--------+--------+")
-    print("\t| ID     | Name               | Age    | Mark   |")
-    print("\t+--------+--------------------+--------+--------+")
+    print("\t+--------+----------+--------------------+--------+--------+")
+    print("\t| ID     | Std_code | Name               | Age    | Score  |")
+    print("\t+--------+----------+--------------------+--------+--------+")
 
     for index, student in enumerate(students):
         print(
-            f"\t| {str(index+1).ljust(7)}| {student[0].ljust(19)}| {student[1].ljust(7)}| {student[2].ljust(7)}|"
+            f"\t| {str(index+1).ljust(7)}| {student[0].ljust(9)}| {student[1].ljust(19)}| {student[2].ljust(7)}| {student[3].ljust(7)}|"
         )
 
-    print("\t+--------+--------------------+--------+--------+")
+    print("\t+--------+----------+--------------------+--------+--------+")
+
+
+def helper_search(students, key=None, value=None):
+    result = []
+    for student in students:
+        if key == "name":
+            if value.lower() in student[1].lower():
+                result.append(student)
+        if key == "age":
+            if value == int(student[2]):
+                result.append(student)
+        if key == "score":
+            if value == float(student[3]):
+                result.append(student)
+
+    return result
+
+
+def search_student(students):
+    print("Which student?")
+    print("\t(!) 1. Search By Name")
+    print("\t(!) 2. Search By Age")
+    print("\t(!) 3. Search By Score")
+
+    result = []
+
+    while True:
+        option = input("\t(?) Choice: ")
+        if option == "1":
+            str_of_name = input("Input the characters: ")
+            result = helper_search(students, key="name", value=str_of_name)
+            break
+
+        elif option == "2":
+            num_of_age = int(input("Input the age: "))
+            result = helper_search(students, key="age", value=num_of_age)
+            break
+
+        elif option == "3":
+            num_of_score = float(input("Input the score: "))
+            result = helper_search(students, key="score", value=num_of_score)
+            break
+
+        else:
+            print("Invalid input. Please try again.")
+
+    if result == []:
+        print("Student is not found.")
+    else:
+        print("Students found:")
+        student_display(result)
+
+    return result
 
 
 def main():
@@ -53,126 +102,134 @@ def main():
         # ADD
         elif command == "2":
             print("[2] Add:")
+            std_code = f"{len(students)+1:06d}"
             name = input("Enter name: ")
             age = input("Enter age: ")
-            mark = input("Enter mark: ")
-            students.append([name, age, mark])
+            score = input("Enter score: ")
+            students.append([std_code, name, age, score])
             print("ADDED")
 
         # EDIT
         elif command == "3":
             print("[3] Edit:")
-            index = input("Enter index: ")
-            if int(index) > len(students):
-                print('Student is not found.')
-            else:
-                print(f"Result: {students[int(index) - 1]}")
+            result = []
+            while len(result) != 1:
+                if len(result) == 0:
+                    result = search_student(students)
+                if len(result) > 1:
+                    print("There are many students in the list.")
+                    result = search_student(result)
 
-                print("Which property need to be updated? ")
-                print("\t(!) 1. Name")
-                print("\t(!) 2. Age")
-                print("\t(!) 3. Mark")
+            index = students.index(result[0])
+            print("Which property need to be updated?")
+            print("\t(!) 1. Name")
+            print("\t(!) 2. Age")
+            print("\t(!) 3. Score")
 
-                while True:
-                    option = input("\t(?) Choice: ")
-                    if option == "1":
-                        name = input("Please enter name you want to change: ")
-                        students[int(index) - 1][0] = name
-                        break
+            while True:
+                option = input("\t(?) Choice: ")
+                if option == "1":
+                    name = input("Please enter name you want to change: ")
+                    students[index][1] = name
+                    break
 
-                    elif option == "2":
-                        age = input("Please enter age you want to change: ")
-                        students[int(index) - 1][1] = age
-                        break
+                elif option == "2":
+                    age = input("Please enter age you want to change: ")
+                    students[index][2] = age
+                    break
 
-                    elif option == "3":
-                        mark = input("Please enter mark you want to change: ")
-                        students[int(index) - 1][2] = mark
-                        break
+                elif option == "3":
+                    score = input("Please enter score you want to change: ")
+                    students[index][3] = score
+                    break
 
-                    else:
-                        print('Invalid input. Please try again.')
+                else:
+                    print("Invalid input. Please try again.")
 
-                print("EDITED")
+            print("EDITED")
 
         # DELETE
         elif command == "4":
             print("[4] Delete:")
-            index = input("Enter index: ")
-            if int(index) > len(students):
-                print('Student is not found.')
-            else:
-                students.pop(int(index) - 1)
-                print("DELETED")
+            result = []
+            while len(result) != 1:
+                if len(result) == 0:
+                    result = search_student(students)
+                if len(result) > 1:
+                    print("There are many students in the list.")
+                    result = search_student(result)
+
+            while True:
+                confirm = input("Are you sure to delete this student (y/n)? ")
+                if confirm == "y":
+                    index = students.index(result[0])
+                    students.pop(int(index))
+                    print("DELETED")
+                    break
+                elif confirm == "n":
+                    break
+                else:
+                    "Invalid input. Please try again."
 
         # SEARCH
         elif command == "5":
             print("[5] Search:")
-            char = input("Input the characters: ")
-            result = []
-            for student in students:
-                if char.lower() in student[0].lower():
-                    result.append(student)
-
-            student_display(result)
+            search_student(students)
 
         # SORT
         elif command == "6":
             print("[6] Sort:")
             print("\t(!) How to sort?")
-            print("\t(!) 1. By Name")
-            print("\t(!) 2. By Age")
-            print("\t(!) 3. By Mark")
+            print("\t(!) 1. By Name (A-Z)")
+            print("\t(!) 2. By Age (Descending)")
+            print("\t(!) 3. By Score (Descending)")
 
             while True:
                 option = input("\t(?) Choice: ")
                 if option == "1":
-                    students = sorted(students, key=lambda x: x[0].lower())
+                    students.sort(key=lambda x: (x[1].lower(), float(x[3])))
                     break
 
                 elif option == "2":
-                    students = sorted(students, key=lambda x: int(x[1]))
+                    students.sort(key=lambda x: (-int(x[2]), x[1].lower()))
                     break
 
                 elif option == "3":
-                    students = sorted(students, key=lambda x: float(x[2]))
+                    students.sort(key=lambda x: (-float(x[3]), x[1].lower()))
                     break
 
                 else:
-                    print('Invalid input. Please try again.')
+                    print("Invalid input. Please try again.")
 
             student_display(students)
 
         # STATS
         elif command == "7":
             print("[7] Statistics:")
-            sum_of_mark = 0
-            for student in students:
-                sum_of_mark += float(student[2])
-
-            print(f"Diem trung binh ca lop: {round(sum_of_mark/len(students),2)}")
-
+            sum_of_score = 0
             excel = []
             good = []
             bad = []
 
             for student in students:
-                if float(student[2]) >= 8:
+                sum_of_score += float(student[2])
+                if float(student[3]) >= 8:
                     excel.append(student)
-                elif float(student[2]) >= 7 and float(student[2]) < 8:
+                elif float(student[3]) >= 7 and float(student[3]) < 8:
                     good.append(student)
                 else:
                     bad.append(student)
 
+            top_score = max([float(x[3]) for x in excel])
+            top_std = [x[1] for x in excel if float(x[3]) == top_score]
+
+            print(f"Diem trung binh ca lop: {round(sum_of_score/len(students),2)}")
             print(
                 f"Ti le sinh vien gioi: {round(float(len(excel)/len(students)),2):.0%}"
             )
             print(f"Ti le sinh vien kha: {round(float(len(good)/len(students)),2):.0%}")
             print(f"Ti le sinh vien kem: {round(float(len(bad)/len(students)),2):.0%}")
-
-            top_mark = max([float(x[2]) for x in excel])
-            top_std = [x[0] for x in excel if float(x[2]) == top_mark]
-            print(f"Sinh vien diem cao nhat ({top_mark}) la:", ", ".join(top_std))
+            print(f"Sinh vien diem cao nhat ({top_score}) la:", ", ".join(top_std))
 
         # SAVE
         elif command == "8":
@@ -189,7 +246,7 @@ def main():
             break
 
         else:
-            print('Invalid input. Please try again.')
+            print("Invalid input. Please try again.")
 
         input("Press Enter to continue...")
 
